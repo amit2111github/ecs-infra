3a8b2d66e0622de2ceb4e1c8141cf23f61c9a8bc
Amit <amitkumarmandal2111@gmail.com>
2025-06-11 01:28:45 +0530

    (feat): changed script to run app

--- utils/initalizationTask.js
+++ utils/initalizationTask.js
@@ -1,9 +1,10 @@
 import { pool } from "../client/pg.js";
 
 export const initateTask = async () => {
-  return Promise.all([
+  const g = await Promise.all([
     pool.query(
       `create table if not exists users (id serial primary key , name text, email text unique , password text)`
     ),
   ]);
+  console.log(g);
 };
